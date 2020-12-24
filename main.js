3780f8adb31c04e2ed234594277a9f11437f6faf
Orlando Rondanelli Ortiz <orlando.rondanelliortiz@cencosud.cl>
2020-12-24 14:23:00 -0300

    :rocket: add whatsapp

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const axios = require('axios')
+require('dotenv').config()
+const client = require('twilio')()
 var minimist = require('minimist')
 
 var args = minimist(process.argv.slice(2), {
@@ -18,8 +20,28 @@ axios.get('https://www.easycancha.com/api/sports/1/clubs/3/timeslots?date='+ day
     })
     if(ok >0 ){
         console.log('hay cancha')
+        client.messages.create({
+            from: 'whatsapp:+14155238886',
+            body: 'Hay cancha para el'+ day +'a las '+ search +' hrs!',
+            to: 'whatsapp:+56993109650'
+          }).then(message => console.log(message.sid));
+          client.messages.create({
+            from: 'whatsapp:+14155238886',
+            body: 'Marco: Hay cancha para el'+ day +'a las '+ search +' hrs! easycancha.cl',
+            to: 'whatsapp:+56966206070'
+          }).then(message => console.log(message.sid));
     } else {
         console.log('no hay cancha')
+        client.messages.create({
+            from: 'whatsapp:+14155238886',
+            body: 'Seguimos buscando cancha para el'+ day +'a las '+ search +' hrs!!',
+            to: 'whatsapp:+56993109650'
+          }).then(message => console.log(message.sid));
+        client.messages.create({
+            from: 'whatsapp:+14155238886',
+            body: 'Marco, seguimos buscando cancha para el'+ day +'a las '+ search +' hrs!',
+            to: 'whatsapp:+56966206070'
+          }).then(message => console.log(message.sid));
     }
   })
   .catch(error => {
