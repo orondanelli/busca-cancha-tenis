6befa4dec499c445fe69ad3fc46431f279e35278
Orlando Rondanelli Ortiz <orlando.rondanelliortiz@cencosud.cl>
2020-12-25 12:48:24 -0300

    notify just when found court

--- notify.js
+++ notify.js
@@ -33,7 +33,7 @@ if (Object.keys(args).length > 1) {
         }).then(message => console.log(message.sid));
       } else {
         console.log('no hay cancha')
-        client.messages.create({
+        /* client.messages.create({
           from: 'whatsapp:+14155238886',
           body: 'Seguimos buscando cancha para el' + day + 'a las ' + search + ' hrs!!',
           to: 'whatsapp:+56993109650'
@@ -42,7 +42,7 @@ if (Object.keys(args).length > 1) {
           from: 'whatsapp:+14155238886',
           body: 'Marco, seguimos buscando cancha para el' + day + 'a las ' + search + ' hrs!',
           to: 'whatsapp:+56966206070'
-        }).then(message => console.log(message.sid));
+        }).then(message => console.log(message.sid)); */
       }
     })
     .catch(error => {
